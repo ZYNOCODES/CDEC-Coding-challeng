73b149bc6a5a9ca3e89cf4a63ff32a0dec0c4640
Fedlallah Mohamed Yacine <127243578+fmyacine@users.noreply.github.com>
2024-03-28 00:02:01 +0100

    Add files via upload
    third challenge

---
+++ math.py
@@ -0,0 +1,21 @@
+import math
+
+a = int(input("give first number: "))
+
+b = int(input("give second number: "))
+
+print("Addition: " +str(a+b))
+
+print("substraction: " +str(a-b))
+
+print("multiplication: " +str(a*b))
+
+print("division: " +str(a/b))
+
+print("first number power second number: " +str(a**b))
+
+print("second number power first number: " +str(b**a))
+
+print("first number factorial: " +str(math.factorial(a)))
+
+print("second number factorial: " +str(math.factorial(b)))
